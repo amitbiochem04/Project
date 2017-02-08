bab598c2a74f5a2026ac07c57cc261d2b9899ad1
github <amitbiochem04@gmail.com>
2017-02-08 16:54:45 +0100

    Regular.sh

---
+++ Regular.sh
@@ -0,0 +1,15 @@
+
+#! -she bang sign 
+##chek which shell
+cat/etc/shell
+echo #SHELL
+cd /etc/init.d/
+
+#!/bin/bash
+####excute permsiion chmod+x scrpt.sh
+## debueg check 
+#!/bin/bash -x
+
+##variable define :variable is assigned to a set of data 
+NAME=DATA
+echo$myname
